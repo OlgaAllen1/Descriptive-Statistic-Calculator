6b1b75807b5f5de4ac6af09cda8a4d56344b3405
Olga Allen <32139467+OlgaAllen1@users.noreply.github.com>
2023-06-30 20:33:32 -0600

    Add project files.

---
+++ Descriptive Statistic Calculator.cpp
@@ -0,0 +1,16 @@
+// Descriptive Statistic Calculator.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+using namespace std;
+#include <iostream>
+
+int main()
+{
+    const int MAX_SIZE = 100;
+    int size = 0;
+    int data[MAX_SIZE];
+
+    cout << "Enter data point (leave blank to stop): ";
+
+    
+}
+
